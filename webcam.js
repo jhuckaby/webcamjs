dd413a18c8ab562fee4470c666fa8ccaf823dc6e
Richard Huang <flyerhzm@gmail.com>
2019-10-20 08:23:57 +0800

    looks getUserMedia is supported in iOS 12

--- webcam.js
+++ webcam.js
@@ -92,10 +92,6 @@ var Webcam = {
 		window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 		this.userMedia = this.userMedia && !!this.mediaDevices && !!window.URL;
 		
-		if (this.iOS) {
-			this.userMedia = null;
-		}
-		
 		// Older versions of firefox (< 21) apparently claim support but user media does not actually work
 		if (navigator.userAgent.match(/Firefox\D+(\d+)/)) {
 			if (parseInt(RegExp.$1, 10) < 21) this.userMedia = null;
